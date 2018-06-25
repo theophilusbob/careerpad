ed340db269b036f255d81521b40a213763239865
Boby Haryanto <thebobyharyanto@gmail.com>
2018-06-25 17:12:19 +0700

    Remove main element, remove import index.css

--- src/Main.js
+++ src/Main.js
@@ -4,11 +4,11 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
 
 const Main = () => (
-    <main>
+    
         <Switch>
             <Route exact path='/' component={Home}/>
         </Switch>
-    </main>
+   
 )
 
 export default Main
