44267a2550c742f736262a1f8013fd23d921538d
Boby Haryanto <thebobyharyanto@gmail.com>
2018-06-27 13:53:07 +0700

    Add How It Works page

---
+++ src/Process.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import arrow from './assets/arrow.svg'
+
+const Process = () => (
+    <div className="main_container">
+                <div className="two_container">
+                    <div className="contentItem">
+                        <h1>How It Works</h1>
+                    </div>
+                    <div className="thickSeparator"/>
+                    <div className="contentItem process">
+                        <p>
+                        Portfolio Submission 
+                        </p>
+                    </div>
+                    <div className="contentItem arrow">
+                        <img src={arrow} />
+                    </div>
+                    <div className="contentItem process">
+                        <p>
+                        Team Verification 
+                        </p>
+                    </div>
+                    <div className="contentItem arrow">
+                        <img src={arrow} />
+                    </div>
+                    <div className="contentItem process">
+                        <p>
+                        Winning Team Announcement 
+                        </p>
+                    </div>
+                </div>
+                <div className="two_container">
+                <div className="contentItem">
+                        <h1>CareerPad Team</h1>
+                    </div>
+                    <div className="thickSeparator"/>
+                    <div className="contentItem process">
+                        <p>
+                        Receive Brief 
+                        </p>
+                    </div>
+                    <div className="contentItem arrow">
+                        <img src={arrow} />
+                    </div>
+                    <div className="contentItem process">
+                        <p>
+                         Mentorship 
+                        </p>
+                    </div>
+                    <div className="contentItem arrow">
+                        <img src={arrow} />
+                    </div>
+                    <div className="contentItem process">
+                        <p>
+                        Project Delivery 
+                        </p>
+                    </div>
+                </div> 
+            </div>
+);
+            
+export default Process
