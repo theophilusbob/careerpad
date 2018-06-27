ced23fc80a6a0fc560b060ee2fd61e097c0063ca
Boby Haryanto <thebobyharyanto@gmail.com>
2018-06-27 13:59:05 +0700

    Update Link to NavLink

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 import ResponsiveMenu from 'react-responsive-navbar';
 import './home.css';
 
 const Header = () => (
     <div className="header">
         <div className="two_container">
-            <div className="logo">CareerPad</div>
+            <div className="logo"><Link className="link" to="/">CareerPad</Link></div>
                 <div className="logo_lite">
                     <h1>C</h1>
                     <div className="logo_underline"></div>
@@ -33,12 +34,12 @@ const Header = () => (
                     smallMenuClassName="small-menu-classname"
                     menu={
                         <ul className="menu">
-                            <li>Vision</li>
-                            <li>Our Process</li>
-                            <li>Founder</li>
-                            <li>About</li>
+                            <li><NavLink className="link" activeClassName="active" to="/about">About</NavLink></li>                          
+                            <li><NavLink className="link" activeClassName="active" to="/mission">Mission</NavLink></li>
+                            <li><NavLink className="link" activeClassName="active" to="/founder">Founder</NavLink></li>
+                            <li><NavLink className="link" activeClassName="active" to="/how-it-works">How It Works</NavLink></li>
                         </ul>
-                    }
+                }
             />
         </div>         
     </div>
