e1dcdbf7138c91521a46a2d387b63902db01efe5
Boby Haryanto <thebobyharyanto@gmail.com>
2018-06-27 13:51:27 +0700

    Add Mission page

---
+++ src/Mission.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Mission = () => (
+    <div className="main_container">
+                <div className="two_container">
+                    <div className="contentItem">
+                        <h1>Mission</h1>
+                    </div>
+                    <div className="thickSeparator"/>
+                    <div className="contentItem">
+                        <p>
+                        Facilitate University Students to have a real work experience by  solving real project and business case.
+                        </p>
+                    </div>
+                    <div className="separator"></div>
+                    <div className="contentItem">
+                        <p>
+                        Enable brands and agencies to find the best talents through accountable programs and supervised by the best mentors in the industry.
+                        </p>
+                    </div>
+                    <div className="separator"></div>
+                    <div className="contentItem">
+                        <p>
+                        Enable brands and agencies to find the best talents through accountable programs and supervised by the best mentors in the industry.
+                        </p>
+                    </div>
+                </div>
+                <div className="two_container">
+                </div> 
+            </div>
+);
+            
+export default Mission
