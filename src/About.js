c3274a772bfe7cb70d95ba4e2bb9f748c8adba51
Boby Haryanto <thebobyharyanto@gmail.com>
2018-06-27 13:50:38 +0700

    Add About page

---
+++ src/About.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const About = () => (
+    <div className="full_container">
+    <div className="main_container">
+                <div className="two_container">
+                    <div className="contentItem">
+                        <h1>About</h1>
+                    </div>
+                    <div className="thickSeparator"/>
+                    <div className="contentItem">
+                        <p>
+                        The premier collaboration platform that connect all the stakeholders of Indonesian digital industry to develop and nurture digital talents in order to overcome the talent shortage. 
+                        </p>
+                    </div>
+                    <div className="contentItem">
+                        <p>
+                        Careerpad is created by the industry players, for the advancement of digital ecosystem in the country. 
+                        </p>
+                    </div>
+                </div>
+                <div className="two_container">
+                </div> 
+        </div>
+    </div>
+);
+            
+export default About
