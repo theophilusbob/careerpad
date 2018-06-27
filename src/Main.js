8d6c09fdcc117edfe7fb22237be9cd5853d99531
Boby Haryanto <thebobyharyanto@gmail.com>
2018-06-27 13:59:37 +0700

    Add Route in Main.js

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,19 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from './Home'
+import About from './About'
+import Mission from './Mission'
+import Founder from './Founder'
+import Process from './Process'
 
 const Main = () => (
     
         <Switch>
             <Route exact path='/' component={Home}/>
+            <Route path='/about' component={About}/>
+            <Route path='/mission' component={Mission}/>
+            <Route path='/founder' component={Founder}/>
+            <Route path='/how-it-works' component={Process}/>
         </Switch>
    
 )
